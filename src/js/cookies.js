d01fb4d24a36425e3224a4af10beca45dbb79e84
Belacanz12 <belacanz@yahoo.com>
2020-06-02 10:51:52 +0300

    no message

--- src/js/cookies.js
+++ src/js/cookies.js
@@ -3,7 +3,7 @@ import $ from 'jquery';
 $(document).ready(function(){
 
   setTimeout(function(){
-    $('.cookies').css('display', 'flex');
+    // $('.cookies').css('display', 'flex');
   }, 8000)
 
   $('.cookies_button').on('click', function(){
